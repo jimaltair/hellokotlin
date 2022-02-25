15e2692959c9b2490ddcb166a8f5983403c0096d
Igor Mitskevich <imi@kode.ru>
2022-02-25 10:47:31 +0200

    page 110

---
+++ src/main/kotlin/kotlincode/chapter4/InnerAndNestedClasses.kt
@@ -0,0 +1,42 @@
+package kotlincode.chapter4
+
+import java.io.Serializable
+
+interface State : Serializable
+
+interface View {
+    fun getCurrentState(): State
+    fun restoreState(state: State) {}
+}
+
+/**
+ * Реализация интерфейса View в Kotlin с помощью вложенного класса.
+ * В Kotlin вложенный класс без модификаторов — это полный аналог статического вложенного класса в Java.
+ * Чтобы превратить его во внутренний класс со ссылкой на внешний класс, нужно добавить модификатор inner.
+ */
+class Button1 : View {
+    override fun getCurrentState(): State = ButtonState()
+
+    override fun restoreState(state: State) {/*...*/
+    }
+
+    class ButtonState : State {/*...*/ }        // Это аналог статического вложенного класса в Java
+}
+
+/**
+ * Соответствие между внутренними и вложенными классами в Java и Kotlin
+ *
+ * Класс А, объявленный внутри другого класса В                 В Java              В Kotlin
+ *
+ * Вложенный класс (не содержит ссылки на внешний класс)    static class A          class A
+ * Внутренний класс (содержит ссылку на внешний класс)      class A                 inner class A
+ */
+
+/**
+ * Чтобы получить доступ к классу Outer из класса Inner, нужно написать this@Outer.
+ */
+class Outer {
+    inner class Inner {
+        fun getOuterReference(): Outer = this@Outer
+    }
+}
---
+++ src/main/kotlin/kotlincode/chapter4/SealedClasses.kt
@@ -0,0 +1,41 @@
+package kotlincode.chapter4
+
+/**
+ * Реализация выражения как интерфейса
+ */
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+/**
+ * При вычислении выражения с использованием конструкции when компилятор Kotlin вынуждает добавить ветку,
+ * выполняемую по умолчанию. В этом примере невозможно вернуть что-либо осмысленное, поэтому генерируется исключение.
+ */
+fun eval(e: Expr): Int =
+    when (e) {
+        is Num -> e.value
+        is Sum -> eval(e.left) + eval(e.right)
+        else -> throw IllegalArgumentException("Unknown expression")        // необходимо также проверять ветку else
+    }
+
+/**
+ * Выражения в виде запечатанных (sealed) классов. Достаточно добавить модификатор sealed в объявление суперкласса, и
+ * он ограничит возможность создания подклассов. Прямые подклассы запечатанных классов и интерфейсов должны быть
+ * объявлены в одном пакете. Никакие другие подклассы не могут появиться после компиляции модуля с запечатанным классом.
+ */
+sealed class Expression
+class Number(val value: Int) : Expression()
+class Summing (val left: Expression, val right: Expression) : Expression()
+
+/**
+ * При обработке всех подклассов запечатанного класса в выражении "when" нет необходимости в ветке по умолчанию.
+ */
+fun evaluate(e: Expression): Int =
+    when (e) {
+        is Number -> e.value
+        is Summing -> evaluate(e.left) + evaluate(e.right)
+    }
+
+/**
+ * Обратите внимание: модификатор sealed означает, что класс по умолчанию открыт, добавлять модификатор open не требуется.
+ */
