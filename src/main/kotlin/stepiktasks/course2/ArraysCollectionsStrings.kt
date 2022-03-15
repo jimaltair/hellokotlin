6df4119f9834785ca2e3a9459a9c227d9748e7e7
Igor Mitskevich <imi@kode.ru>
2022-03-15 16:27:12 +0200

    start fun findSubstring()

--- src/main/kotlin/stepiktasks/course2/ArraysCollectionsStrings.kt
+++ src/main/kotlin/stepiktasks/course2/ArraysCollectionsStrings.kt
@@ -1,5 +1,7 @@
 package stepiktasks.course2
 
+import java.util.*
+
 /**
  * Необходимо разработать программу для поиска среднего и медианного значений списка действительных чисел со знаком,
  * подаваемого на вход программы. Медианным значением считается такое число выборки, что ровно половина из элементов
@@ -22,6 +24,77 @@ package stepiktasks.course2
  * Sample Output 2:
  * 3.25 3.0
  */
-fun main(args: Array<String>) {
+//fun main(args: Array<String>) {
+//    val input = readLine()!!
+//        .split(" ")
+//        .map { it.toDouble() }
+//        .sorted()
+//
+//    val average = input.average()
+//
+//    val size = input.size
+//    val median = when {
+//        size % 2 == 0 -> (input[size / 2 - 1] + input[size / 2]) / 2
+//        else -> input[size / 2]
+//    }
+//    println("$average $median")
+//}
 
+/**
+ * Вам надо написать функцию для удаления повторных регистраций от пользователей с одинаковыми именами.
+ */
+fun deleteCopies(inputList: List<String>) : List<String> {
+    return inputList.distinct()
+}
+
+/**
+ * На вход функции подаётся список фамилий учеников (могут быть на русском, английском языках). Программа должна
+ * возвращать список, каждый элемент которого содержит фамилии учеников, начинающиеся с одной и той же буквы.
+ *
+ * Sample Input:
+ * Smith Jones Bambury Patel Brown Singh Williams Taylor Wilson Davies Evans Scott
+ *
+ * Sample Output:
+ * Bambury Brown
+ * Davies
+ * Evans
+ * Jones
+ * Patel
+ * Scott Singh Smith
+ * Taylor
+ * Williams Wilson
+ */
+//fun main(args: Array<String>){
+//    val inputList = readLine()!!.split(" ")
+//    val sortedLists = sortLists(inputList)
+//    for (list in sortedLists){
+//        for (string in list) {
+//            print("$string ")
+//        }
+//        println()
+//    }
+//}
+
+fun sortLists(inputList: List<String>) : MutableList<MutableList<String>> {
+    return inputList
+        .groupBy { it.first() }
+        .values
+        .map { it.toMutableList().apply { sort() } }
+        .toMutableList().apply { sortBy { it.first().first() } }
+}
+
+/**
+ * На вход программа получается документ, представленный строкой, а также строку для поиска. На выход программа выдаёт
+ * все вхождения строки для поиска в документ (индексы начального символа вхождения по возрастанию). Заметим, что
+ * предполагается, что вхождения не могут пресекаться (т.е. в случае поиска строки "абракадабра" в документе
+ * "абракадабрабракадабра" будет обозначено только одно вхождение, первое слева).
+ *
+ * Sample Input:
+ * абракадабрабракадабра абракадабра
+ *
+ * Sample Output:
+ * 0
+ */
+fun findSubstrings(text: String, substring: String) : MutableList<Int> {
+    TODO()
 }
