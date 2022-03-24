f4579c61cf106b925838f1c070edd85dedfab613
Igor Mitskevich <jimaltair1987@gmail.com>
2022-03-24 17:44:15 +0200

    starting metrics task

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.6.10")
     implementation("com.google.guava:guava:30.1.1-jre")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.http4k:http4k-core:4.20.1.0")
 }
 
 tasks.test {
